96d223fb73ef2b5d34dfe706d2b9a6833416c484
kurtwilkinson <kurtwilkinson024@yahoo.com>
2024-02-01 10:28:39 +0800

    Add files via upload

---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+import './bootstrap';
+
+import {createApp} from 'vue'
+
+import App from './item/list.vue'
+
+createApp(App).mount("#app")
---
+++ storage/framework/views/a09a077fd67079057914bb9bb420bfa47c3ca886.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php echo app('Illuminate\Foundation\Vite')('resources/css/app.css'); ?>
+    </head>
+    <body>
+        <div id="app"></div>
+        <?php echo app('Illuminate\Foundation\Vite')('resources/js/app.js'); ?>
+    </body>
+</html>
+<?php /**PATH /home/kurt/Projects/Laravel/ecommerce/resources/views/welcome.blade.php ENDPATH**/ ?>
